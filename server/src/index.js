3e4649f59c7118e2fd7d28ba92575ebba42a5ad4
alurakhil24 <alur.akhil@gmail.com>
2019-07-24 09:59:03 +0530

    added more to demo

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,16 @@ const socketio = require('socket.io')
 const server = app.listen(5000);
 console.log('server listening on port 5000');
 const io = socketio(server);
-io.on('connection',(socket)=>{
-    socket.emit('fromServer',{message:"Hey buddy you are connected"});
-    socket.on('fromClient',(clientData)=>{
+io.on('connection', (socket) => {
+    socket.emit('fromServer', { message: "Hey buddy you are connected" });
+    socket.on('fromClient', (clientData) => {
         console.log(clientData)
-        socket.emit('fromServer', {message: "Thats ok bro.. Enjoy with Sockets"})
+        socket.emit('fromServer', { message: "Thats ok bro.. Enjoy with Sockets" })
     })
-    setTimeout(() => {
-        socket.emit('fromServer', {message: 'This is the second message you will get after 2 seconds '})
+    const interval = setInterval(() => {
+        socket.emit('fromServer', { message: 'This is the message you will get after 2 seconds for 5 times ' })
     }, 2000);
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 12000);
 })
